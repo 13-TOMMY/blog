b9255123eb47740131d1efffcc8a531f5a570140
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-04 14:45:26 +0200

    week 3/1

--- src/components/Likes/Likes.jsx
+++ src/components/Likes/Likes.jsx
@@ -12,34 +12,78 @@ import {
   where,
 } from "firebase/firestore";
 
-function Likes({ articleId }) {
+const Likes = ({ articleId }) => {
   const [user] = useAuthState(auth);
+
   const [isLiked, setIsLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
 
   useEffect(() => {
-    const likesRef = collection(db, "Likes");
+    const likesRef = collection(db, "likes");
     if (user) {
       const q = query(
         likesRef,
         where("articleId", "==", articleId),
         where("userId", "==", user?.uid)
       );
+
       getDocs(q, likesRef)
         .then((res) => {
           if (res.size > 0) {
             setIsLiked(true);
           }
         })
-        .catch((err) => console.error(err));
+        .catch((err) => console.log(err));
     }
   }, [user]);
 
   useEffect(() => {
     const likesRef = collection(db, "likes");
-  });
-  const handleLikes = (e) => {};
-  const handleUnlike = (e) => {};
+
+    const q2 = query(likesRef, where("articleId", "==", articleId));
+
+    getDocs(q2, likesRef)
+      .then((res) => {
+        setLikeCount(res.size);
+      })
+      .catch((err) => console.log(err));
+  }, [isLiked]);
+
+  const handleLikes = (e) => {
+    if (user) {
+      const likesRef = collection(db, "likes");
+      addDoc(likesRef, {
+        userId: user?.uid,
+        articleId: articleId,
+      })
+        .then((res) => {
+          setIsLiked(true);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const handleUnlike = (e) => {
+    if (user) {
+      const likesRef = collection(db, "likes");
+
+      const q = query(
+        likesRef,
+        where("articleId", "==", articleId),
+        where("userId", "==", user?.uid)
+      );
+
+      getDocs(q, likesRef)
+        .then((res) => {
+
+          const likesId = res.docs[0].id;
+          deleteDoc(doc(db, "likes", likesId))
+            .then((res) => setIsLiked(false))
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <div>
       {isLiked ? (
