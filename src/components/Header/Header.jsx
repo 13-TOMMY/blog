609916b35395bbfc9429d91a21180e460a6dc08e
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-05 10:35:18 +0200

    css for header, new font and colors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,32 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { auth } from "../../config/firebaseCongfig";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
 import "./Header.css";
 
-
 function Header() {
   const [user] = useAuthState(auth);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const categories = ["Health", "Food", "Travel", "Technology"];
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-  const navigate = useNavigate();
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <div className="header-container">
-      <FaHome className="nav-link home-btn" onClick={() => navigate("/")} />
+      <FaHome
+        className="home-btn"
+        onClick={() => {
+          setSelectedCategory(null); 
+          navigate("/");
+        }}
+      />
       {user && (
         <Link to="/addArticle" className="auth-link">
           Add Article
@@ -23,7 +34,16 @@ function Header() {
       )}
       <div className="categories-container">
         {categories.map((item, index) => (
-          <Link to={`/category/${item}`} className="nav-link" key={index}>
+          <Link
+            to={`/category/${item}`}
+            className={`nav-link ${
+              location.pathname === `/category/${item}`
+                ? "selected-category"
+                : ""
+            }`}
+            key={index}
+            onClick={() => handleCategoryClick(item)}
+          >
             {item}
           </Link>
         ))}
@@ -33,13 +53,22 @@ function Header() {
           <span className="username">
             {user.displayName ? user.displayName : user.email}
           </span>
-          <button className="auth-link" onClick={() => signOut(auth)}>
-            Log out
+          <button
+            className="auth-link"
+            onClick={() => {
+              signOut(auth);
+              setSelectedCategory(null);
+            }}
+          >
+            Log Out
           </button>
         </div>
       ) : (
-        <Link className="auth-link" to={"/auth"}>
-          Signup
+        <Link
+          className="auth-link"
+          to={"/auth"}
+        >
+          Sign Up
         </Link>
       )}
     </div>
